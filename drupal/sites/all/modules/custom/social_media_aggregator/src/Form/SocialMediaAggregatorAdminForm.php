2ac354c1b07d493bb0eb6166aaac87d543a8eab1
Edward Chan <edwardchan1350@yahoo.com>
2016-01-27 11:51:57 -0500

    Add Form directory including SocialMediaAggregatorForm class

---
+++ drupal/sites/all/modules/custom/social_media_aggregator/src/Form/SocialMediaAggregatorAdminForm.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\example\Form\ExampleForm.
+ */
+
+namespace Drupal\example\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Implements an example form.
+ */
+class SocialMediaAggregatorForm extends FormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'socialmediaaggregator_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $form['social_media_aggregator_admin'] = array(
+      '#type' => 'tel',
+      '#title' => $this->t('Your phone number'),
+    );
+    $form['actions']['#type'] = 'actions';
+    $form['actions']['submit'] = array(
+      '#type' => 'submit',
+      '#value' => $this->t('Save'),
+      '#button_type' => 'primary',
+    );
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    if (strlen($form_state->getValue('social_media_aggregator_admin')) < 3) {
+      $form_state->setErrorByName('social_media_aggregator_admin', $this->t('The phone number is too short. Please enter a full phone number.'));
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    drupal_set_message($this->t('Your phone number is @number', array('@number' => $form_state->getValue('social_media_aggregator_admin'))));
+  }
+
+}
+?>
